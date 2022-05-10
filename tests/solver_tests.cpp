f993dae149f41c245760e26f90b48ac965d5dbf5
Sergey Sysoev <Sergey.Sysoev@jetbrains.com>
2022-05-11 01:40:33 +0300

    Push failed test to check that CI is working

--- tests/solver_tests.cpp
+++ tests/solver_tests.cpp
@@ -7,7 +7,7 @@ using namespace solver;
 TEST_SUITE("solver") {
     TEST_CASE("test") {
         SUBCASE("==5") {
-            CHECK_EQ(5, test());
+            CHECK_EQ(4, test());
         }
     }
 }
