8d6447762aad0565b7847ff4e83d3d5e90ff169f
Sergey Sysoev <Sergey.Sysoev@jetbrains.com>
2022-06-04 00:01:48 +0300

    Merge CNF.h

--- solver/include/CNF.h
+++ solver/include/CNF.h
@@ -10,17 +10,14 @@
 #include "utils.h"
 
 
-namespace CNF
-{
-	enum class Value
-	{
+namespace CNF {
+	enum class Value {
 		False = 0,
 		True = 1,
 		Undefined = 2
 	};
 
-	struct Variable
-	{
+	struct Variable {
 		int id;
 		Value value;
 
@@ -28,52 +25,47 @@ namespace CNF
 		// int count_in_literals_with_negate;
 		// int count_in_literals_without_negate;
 
-		explicit Variable(int id = -1, Value value = Value::Undefined) : id(id), value(value)
-		{}
+		explicit Variable(int id = -1, Value value = Value::Undefined) : id(id), value(value) {}
 	};
 
-	struct Literal
-	{
+	struct Literal {
 		std::shared_ptr<Variable> var;
 		bool has_negate;
 
-		Literal(std::shared_ptr<Variable> var, bool has_negate) : var(std::move(var)), has_negate(has_negate)
-		{}
+		Literal(std::shared_ptr<Variable> var, bool has_negate) : var(std::move(var)), has_negate(has_negate) {}
 
 		[[nodiscard]] Value get_value() const;
 	};
 
-	class Clause
-	{
+	class Clause {
 	public:
-		explicit Clause(std::vector<Literal> literals = {}) : literals(std::move(literals)), isTrue(false)
-		{}
+		explicit Clause(std::vector<Literal> literals = {}) : literals(std::move(literals)) {}
 
-		Clause(const Clause& other) {
+		Clause(const Clause &other) {
 			literals.clear();
-			isTrue = false;
+			watched_literals = other.watched_literals;
 
 			literals = other.literals;
 		}
 
-		Clause(Clause && other) noexcept {
+		Clause(Clause &&other) noexcept {
 			literals.clear();
-			isTrue = false;
+			watched_literals = other.watched_literals;
 
-			literals = other.literals;
+			literals = std::move(other.literals);
 		}
 
-		Clause &operator =(const Clause& other) {
+		Clause &operator=(const Clause &other) {
 			literals.clear();
-			isTrue = false;
+			watched_literals = other.watched_literals;
 
 			literals = other.literals;
 			return *this;
 		}
 
-		Clause &operator =(Clause && other) noexcept {
+		Clause &operator=(Clause &&other) noexcept {
 			literals.clear();
-			isTrue = false;
+			watched_literals = other.watched_literals;
 
 			literals = other.literals;
 
@@ -82,7 +74,7 @@ namespace CNF
 
 		[[nodiscard]] CNF::Value get_value() const {
 			bool has_undefined = false;
-			for (const auto &lit : literals) {
+			for (const auto &lit: literals) {
 				if (lit.get_value() == Value::Undefined) {
 					has_undefined = true;
 				}
@@ -98,16 +90,19 @@ namespace CNF
 		bool needs_attention();
 
 		std::array<Literal, 2> get_watched_literals();
+
 	private:
-		int watched_literals[2] = {0, 1};
+		std::array<int, 2> watched_literals = {0, 1};
 
 		utils::Maybe<int> find_first_non_false_literal(int excluded_index);
 
 	};
 }
 
-std::ostream& operator<<(std::ostream& out, const CNF::Variable& var);
-std::ostream& operator<<(std::ostream& out, const CNF::Literal& literal);
-std::ostream& operator<<(std::ostream& out, const CNF::Clause& clause);
+std::ostream &operator<<(std::ostream &out, const CNF::Variable &var);
+
+std::ostream &operator<<(std::ostream &out, const CNF::Literal &literal);
+
+std::ostream &operator<<(std::ostream &out, const CNF::Clause &clause);
 
 #endif //CDCL_SOLVER_CNF_H
